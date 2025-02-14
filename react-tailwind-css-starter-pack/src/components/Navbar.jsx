af90aebf8dabbfbb73a19aabf3500d3cc8849141
Abir Maji <112890292+Abir-Maji@users.noreply.github.com>
2025-02-14 11:49:40 +0530

    Create Navbar.jsx

---
+++ react-tailwind-css-starter-pack/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+  return (
+    <nav class="block w-full max-w-screen-lg px-4 py-2 mx-auto bg-white shadow-md rounded-md lg:px-8 lg:py-3 mt-10">
+  <div class="container flex flex-wrap items-center justify-center mx-auto text-slate-800">
+    <a href="#" class="py-1.5 text-base text-slate-800 font-semibold">
+      Enterprise Expense Management System
+    </a> 
+    
+  </div>
+  
+  
+</nav>
+  );
+};
+
+export default Navbar;
